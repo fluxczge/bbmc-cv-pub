640ecd910efdac9d2c80d4c8be572ba7b703e046
Brian <bobby_brian_cipriano@manulife.com>
2020-04-13 16:34:01 +0800

    initial

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  hideAll();
+  $("#homeDiv").show();
+    
+  function hideAll() {
+    $("#homeDiv").hide();
+    $("#resumeDiv").hide();
+    $("#skillsDiv").hide();
+    $("#projectDiv").hide();
+  }
+
+  $("#homeNav").click(function(){
+    hideAll();
+    $("#homeDiv").show();
+  });
+
+  $("#resumeNav").click(function(){
+    hideAll();
+    $("#resumeDiv").show();
+  });
+
+  $("#skillsNav").click(function(){
+    hideAll();
+    $("#skillsDiv").show();
+  });
+
+  $("#projectNav").click(function(){
+    hideAll();
+    $("#projectDiv").show();
+  });
+
+});
